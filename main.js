da6c5253a27d89f4c7bc48782b460572c82882fe
Dimi Dimov <Dimi Dimov>
2017-02-28 23:33:03 -0800

    its fine

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-
+alert("1st part")
 })
     $.get("/ping", function(data){
         if(data.error == "true"){
@@ -9,24 +9,23 @@ $(function(){
     }, "json")
 
     $("#submit1").click(function(){
-      login(1);
-    });
-    $("#submit2").click(function(){
-      login(2);
+      alert("it worked!")
+      submitData();
     });
 
 
-    function login(index){
-      $.post("/login"+index, {username: $("#username"+index).val(), password: $("#password"+index).val()})
-        .done(function(data){
-          if(data.result == "failed"){
+    function submitData(){
+      $.post("/submitData1", {username: $("#username").val(), password: $("#level"+index).val()})
+        //.done(function(data){
+          /*if(data.result == "failed"){
             console.log(data)
-            $("#result"+index).text("Failed to login! " + data.message);
+            $("#result").text("Failed to login! " + data.message);
           } else {
             console.log(data)
             $("#result"+index).text("Logged in as: " + data.username + (data.randomCode ? " (CODE: " + data.randomCode + ")" : ""));
           }
-        });
+          */
+        //};
     }
 
 
